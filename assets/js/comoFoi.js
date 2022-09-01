ec68026f8eed2b8702f4952857e72f1283e7c1b5
Bruno Siqueira <104639923+brunosiq99@users.noreply.github.com>
2022-09-01 11:43:35 -0300

    Update comoFoi.js

--- assets/js/comoFoi.js
+++ assets/js/comoFoi.js
@@ -8,21 +8,21 @@ let currentOpenedImage = '';
 function putsImageInDayDiv() {
     // DOMINGO
     dayDiv = document.getElementById('images_domingo'); 
-    fetch(`http://127.0.0.1:5500/assets/json/${dayDiv.id}.html`).
+    fetch(`https://brunosiq99.github.io/encontro-motociclistas-tc-2/assets/json/${dayDiv.id}.html`).
         then((response) => {    
             createImages(response,dayDiv)
         }).
         then(()=>{
             // SÁBADO
             dayDiv = document.getElementById('images_sabado');
-            fetch(`http://127.0.0.1:5500/assets/json/${dayDiv.id}.html`).
+            fetch(`https://brunosiq99.github.io/encontro-motociclistas-tc-2/assets/json/${dayDiv.id}.html`).
                 then((response) => {
                     createImages(response,dayDiv)
                 }).
                 then(()=>{
                     // SEXTA
                     dayDiv = document.getElementById('images_sexta');
-                    fetch(`http://127.0.0.1:5500/assets/json/${dayDiv.id}.html`).
+                    fetch(`https://brunosiq99.github.io/encontro-motociclistas-tc-2/assets/json/${dayDiv.id}.html`).
                         then((response) => { 
                             createImages(response,dayDiv)
                         }).
