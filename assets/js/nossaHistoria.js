48bee8fe3a81d6c714ab0c942cbd5ed6b437c7a6
Bruno Siqueira <104639923+brunosiq99@users.noreply.github.com>
2022-09-28 15:30:45 -0300

    new page 'nossa história'
    new page 'nossa história' containing:
    - slide banner showing the five event editions 
    - text and image detailing each edition
    
    Creating a module for moving slides exported and used in two subpages with the functionality

---
+++ assets/js/nossaHistoria.js
@@ -0,0 +1,39 @@
+
+//slide banner creation
+import {setSlidesControlButton, activeOrDesactiveSlidesButtons} from './moveSlide.js';
+const bannerDiv = document.querySelector('.banner__content');
+const bannerSlider = document.querySelector('.banner__slides');
+
+let bannerSlides = [];
+let bannerSliderWidth = 0;
+function loadBanner(banner){
+    const bannerSlide = document.createElement('div');
+    bannerSlides.push(bannerSlide);
+    bannerSliderWidth += 100;
+    bannerSlide.className = 'banner__slide';
+    bannerSlide.style.backgroundImage = `url(${banner.src})`;
+    const bannerTitle = document.createElement('h3');
+    bannerTitle.className = 'banner__slide___title';
+    bannerTitle.innerText = banner.title;
+
+    bannerSlider.appendChild(bannerSlide);
+    bannerSlide.appendChild(bannerTitle);
+}
+function setSlidesWidthAndMargin(slider,slides,sliderWidth){
+    slider.style.width = sliderWidth+'%';
+    const slideWidth = 10000/sliderWidth;
+    slides.forEach((slide)=>{
+        slide.style.width = slideWidth+'%';
+    })
+}
+fetch('http://127.0.0.1:5500/assets/json/banner-nossa-historia.html').
+    then(data => data.json()).
+    then(response => {
+        response.forEach(banner => loadBanner(banner));
+        setSlidesWidthAndMargin(bannerSlider,bannerSlides,bannerSliderWidth);
+        const minMarginLeft = -bannerSliderWidth + 100;
+        
+        setSlidesControlButton(bannerDiv, minMarginLeft, bannerSlider);
+        activeOrDesactiveSlidesButtons(minMarginLeft)
+    });
+
