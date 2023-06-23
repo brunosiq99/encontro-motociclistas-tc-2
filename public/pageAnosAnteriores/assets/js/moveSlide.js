9876df3b21507b25d24fa38e1387b001fd31481d
Bruno Siqueira <104639923+brunosiq99@users.noreply.github.com>
2023-06-22 21:30:03 -0300

    Add files via upload

--- public/pageAnosAnteriores/assets/js/moveSlide.js
+++ public/pageAnosAnteriores/assets/js/moveSlide.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let bandasSlideMarginLeft = 0;
 function setSlidesControlButton(slider,minMarginLeft,slidesDiv){
     const slideControlButtons = slider.querySelectorAll('.slides__control-buttons');
@@ -42,4 +43,50 @@ function activeOrDesactiveSlidesButtons(minMarginLeft){
     }
 }
 
+=======
+let bandasSlideMarginLeft = 0;
+function setSlidesControlButton(slider,minMarginLeft,slidesDiv){
+    const slideControlButtons = slider.querySelectorAll('.slides__control-buttons');
+    slideControlButtons.forEach((slideButton)=>{
+        slideButton.addEventListener('click',()=>{
+            moveSlide(slidesDiv,slideButton,minMarginLeft);
+        })
+    }) 
+}
+function moveSlide(slidesDiv,slideButton,minMarginLeft){
+    if(slideButton.id === "previous-slide__button"){
+        if(bandasSlideMarginLeft + 100 > 0){
+            bandasSlideMarginLeft = 0;
+        }else{
+            bandasSlideMarginLeft += 100;
+        } 
+    }
+    if(slideButton.id === "next-slide__button"){
+        if(bandasSlideMarginLeft - 100 < minMarginLeft){
+            bandasSlideMarginLeft = minMarginLeft;
+        }else{
+            bandasSlideMarginLeft -= 100;
+        }   
+    }
+    slidesDiv.style.marginLeft = `${bandasSlideMarginLeft}%`;
+    activeOrDesactiveSlidesButtons(minMarginLeft) 
+}
+function activeOrDesactiveSlidesButtons(minMarginLeft){
+    const leftSlideButton = document.querySelector('#previous-slide__button');
+    const rightSlideButton = document.querySelector('#next-slide__button');
+
+    if(bandasSlideMarginLeft === 0){
+        leftSlideButton.style.display = 'none';
+    }else{
+        leftSlideButton.style.display = 'block';
+    }
+
+    if(bandasSlideMarginLeft <= minMarginLeft){
+        rightSlideButton.style.display = 'none';
+    }else{
+        rightSlideButton.style.display = 'block';
+    }
+}
+
+>>>>>>> f1094915a0d9661cb23beeac5b8871355aa98dfb
 export {setSlidesControlButton, moveSlide, activeOrDesactiveSlidesButtons}
--- public/pageAnosAnteriores/assets/js/nossaHistoria.js
+++ public/pageAnosAnteriores/assets/js/nossaHistoria.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 //slide banner creation
 import {setSlidesControlButton, activeOrDesactiveSlidesButtons} from './moveSlide.js';
@@ -37,3 +38,44 @@ fetch('./assets/json/banner-nossa-historia.html').
         activeOrDesactiveSlidesButtons(minMarginLeft)
     });
 
+=======
+
+//slide banner creation
+import {setSlidesControlButton, activeOrDesactiveSlidesButtons} from './moveSlide.js';
+const bannerDiv = document.querySelector('.banner__content');
+const bannerSlider = document.querySelector('.banner__slides');
+
+let bannerSlides = [];
+let bannerSliderWidth = 0;
+function loadBanner(banner){
+    const bannerSlide = document.createElement('div');
+    bannerSlides.push(bannerSlide);
+    bannerSliderWidth += 100;
+    bannerSlide.className = 'banner__slide';
+    bannerSlide.style.backgroundImage = `url(${banner.src})`;
+    const bannerTitle = document.createElement('h3');
+    bannerTitle.className = 'banner__slide___title';
+    bannerTitle.innerText = banner.title;
+
+    bannerSlider.appendChild(bannerSlide);
+    bannerSlide.appendChild(bannerTitle);
+}
+function setSlidesWidthAndMargin(slider,slides,sliderWidth){
+    slider.style.width = sliderWidth+'%';
+    const slideWidth = 10000/sliderWidth;
+    slides.forEach((slide)=>{
+        slide.style.width = slideWidth+'%';
+    })
+}
+fetch('./assets/json/banner-nossa-historia.html').
+    then(data => data.json()).
+    then(response => {
+        response.forEach(banner => loadBanner(banner));
+        setSlidesWidthAndMargin(bannerSlider,bannerSlides,bannerSliderWidth);
+        const minMarginLeft = -bannerSliderWidth + 100;
+        
+        setSlidesControlButton(bannerDiv, minMarginLeft, bannerSlider);
+        activeOrDesactiveSlidesButtons(minMarginLeft)
+    });
+
+>>>>>>> f1094915a0d9661cb23beeac5b8871355aa98dfb
