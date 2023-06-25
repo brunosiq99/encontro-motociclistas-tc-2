aeecbc97b99147358533d6ff823a8b55fa2bfad6
Bruno Siqueira <104639923+brunosiq99@users.noreply.github.com>
2023-06-25 20:30:23 -0300

    setting MyDocument in _documentjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,16 @@ import Partners from '@/src/components/Partners/Partners';
 import Header from '@/src/components/Header/Header';
 import Footer from '@/src/components/Footer/Footer';
 import SlideBanner from '@/src/components/SlideBanner/SlideBanner';
+import Head from 'next/head';
 const Home = ({config}) => {  
     return (
         <div>
+            <Head>
+                <title>Encontro de Motociclistas - Três Corações</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+            </Head>
             <Header 
                 key="HeaderInHome" 
                 config={config} 
