21253463928f9d416e6f4dd3e83da4a644b4f8ef
brunosiq99 <brunosiq99@gmail.com>
2023-07-10 17:19:48 -0300

    adding bands

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a2810b191a87b76"; }
+/******/ 		__webpack_require__.h = function() { return "17370148d44a154d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/_app.fba186cd387db93c.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"pages":[{"name":"Home","path":"/"},{"name":"Anos Anteriores","path":"/anos-anteriores"}],"colorStyles":{"light":{"backgroundBase":"#f9f9f9","backgroundLevel1":"#ffffff","backgroundLevel2":"#f0f0f0","borderBase":"#e5e5e5","textColorBase":"#222222"},"dark":{"backgroundBase":"#181818","backgroundLevel1":"#202020","backgroundLevel2":"#313131","borderBase":"#383838","textColorBase":"#ffffff"}},"partners":{"patrocinadores":[{"name":"Go Mobilidade Urbana","imgFile":"go-mobilidade.png"},{"name":"MotoGerais","imgFile":"motogerais.jpeg"},{"name":"Paddock","imgFile":"paddock.jpeg"},{"name":"Terremoto Motopeças","imgFile":"terremoto.jpeg"},{"name":"Bolota Motopeças","imgFile":"bolota.jpg"},{"name":"Scap Centro Automotivo","imgFile":"scap.png"},{"name":"Auto Center WS Rodas","imgFile":"ws-rodas.jpeg"},{"name":"Loja Zuza","imgFile":"loja-zuza.png"},{"name":"Ótica Real","imgFile":"otica-real.jpeg"},{"name":"Trectur","imgFile":"trectur.jfif"},{"name":"Ferragens Três Corações","imgFile":"ferragens-tc.jpeg"},{"name":"Anderson Despachante","imgFile":"anderson-despachante.jpeg"},{"name":"Adega São João","imgFile":"adega-sao-joao.jpeg"},{"name":"Chopperia Vila Alemã","imgFile":"vila-alema.png"},{"name":"Kremer","imgFile":"kremer.png"},{"name":"Pizza da Bia","imgFile":"pizza-da-bia.jpeg"},{"name":"Dogão da Xeninha","imgFile":"dogao-da-xeninha.jpeg"}]},"bands":[{"name":"Rocksauro","img":"rocksauro.jpeg","instagram":"","youtube":""},{"name":"Garibaldos","img":"garibaldos.png","instagram":"","youtube":""},{"name":"Bad Road","img":"bad-road.jpeg","instagram":"","youtube":""},{"name":"Evil Zé","img":"evil-ze.jpg","instagram":"","youtube":""},{"name":"Le Cambada","img":"le-cambada.png","instagram":"","youtube":""},{"name":"Mil Contra","img":"mil-contra.png","instagram":"","youtube":""},{"name":"Pé na Estrada","img":"pe-na-estrada.png","instagram":"","youtube":""},{"name":"Excellence","img":"excellence.png","instagram":"","youtube":""},{"name":"Armação Ilimitada","img":"armacao-ilimitada.png","instagram":"","youtube":""},{"name":"Turbo Rock","img":"turbo-rock.png","instagram":"","youtube":""},{"name":"Black Wolf","img":"black-wolf.jpg","instagram":"","youtube":""},{"name":"Félix Jackson","img":"felix-jackson.png","instagram":"","youtube":""},{"name":"Magno Alves Locutor   ","img":"magno-locutor.jpeg","instagram":"","youtube":""}]}');
+
+/***/ })
+
+});
